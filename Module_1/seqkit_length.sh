082087a24b61e525157469a57dae6dfc01134cfe
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-10-11 13:24:32 +0200

    Update seqkit_length.sh

--- Module_1/seqkit_length.sh
+++ Module_1/seqkit_length.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+###################################################################
+#Script Name	:seqkit_length.sh		                                                                                              
+#Description	:Removes contigs from fasta that are smaller than 200 nt (for upload to TSA)                                                                       
+#Args:                                                                                           
+#Author	:Jennifer Gerbracht                                               
+#Email		:jennifer.gerbracht@gmx.de                                           
+###################################################################
+
+source ../config.txt
+moduledir="${mydir}/Module_1"
+
 seqkit \
 seq \
 -m 200 \
-NA_transcripts.fasta > NA_transcripts_200.fasta
+${moduledir}/NA_transcripts.fasta > ${moduledir}/NA_transcripts_200.fasta
