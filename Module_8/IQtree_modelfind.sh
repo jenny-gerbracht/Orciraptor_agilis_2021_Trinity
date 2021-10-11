11fbd4bf28812c6d9d77bc07a95358cc696094ae
Jennifer Gerbracht <73494618+jenny-gerbracht@users.noreply.github.com>
2021-10-11 15:17:37 +0200

    Update IQtree_modelfind.sh

--- Module_8/IQtree_modelfind.sh
+++ Module_8/IQtree_modelfind.sh
@@ -4,6 +4,14 @@
 #$ -cwd
 #$ -pe threaded 10
 
+###################################################################
+#Script Name	:IQtree.sh		                                                                                              
+#Description	:Find best model for IQtree                                                                              
+#Args:                                                                                           
+#Author	:Jennifer Gerbracht                                               
+#Email		:jennifer.gerbracht@gmx.de                                           
+###################################################################
+
 source activate iqtree
 iqtree \
 -s mafft_alignment_trimal_derep_defrag.phy \
